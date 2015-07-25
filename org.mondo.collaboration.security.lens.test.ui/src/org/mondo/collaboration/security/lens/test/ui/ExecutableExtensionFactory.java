bae03c4ca85749d30b613d94f922438e4e2d2771
bergmann <bergmann@mit.bme.hu>
2015-07-25 19:06:48 +0200

    Added simple testing facility for security lens context

---
+++ org.mondo.collaboration.security.lens.test.ui/src/org/mondo/collaboration/security/lens/test/ui/ExecutableExtensionFactory.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2004-2015 Gabor Bergmann and Daniel Varro
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Gabor Bergmann - initial API and implementation
+ *******************************************************************************/
+
+package org.mondo.collaboration.security.lens.test.ui;
+
+import org.eclipse.incquery.patternlanguage.emf.ui.EMFPatternLanguageExecutableExtensionFactory;
+import org.osgi.framework.Bundle;
+
+/**
+ * @author Bergmann Gabor
+ *
+ */
+public class ExecutableExtensionFactory extends EMFPatternLanguageExecutableExtensionFactory {
+
+	@Override
+	protected Bundle getBundle() {
+		return Activator.getDefault().getBundle();
+	}
+}
