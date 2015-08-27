30158b5a7ab0d9db53f4c2f957c511d13f20105e
Csaba Debreceni <debreceni@mit.bme.hu>
2015-08-27 15:25:04 +0200

    lens executor script became more readable

--- mondo-server-hooks/svn/lens-executor.sh
+++ mondo-server-hooks/svn/lens-executor.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
-# This script is responsible for executing the lens application with the specified arguments
-
-USER="$1"
+# Config variables
+# Set them carefully
 LENS="/home/vialpando/Eclipse/Mondo/lens/eclipse/MondoOfflineCollaborationLens"
 ARGS="-nosplash -consoleLog --launcher.suppressErrors -application org.mondo.collaboration.security.lens.offline.cli"
-GOLD="$2"
-FRONT="$3"
 MACL="/home/vialpando/Eclipse/Mondo/git/mondo-collab-internal/ikerlan-related/es.ikerlan.wt.emf.security.example/src/es/ikerlan/wt/emf/security/example/rules.macl"
 EIQ="/home/vialpando/Eclipse/Mondo/git/mondo-collab-internal/ikerlan-related/es.ikerlan.wt.emf.security.example/src/es/ikerlan/wt/emf/security/example/queries.eiq"
+
+# Input parameters
+USER="$1"
+GOLD="$2"
+FRONT="$3"
+TYPE="$4"
+TEMP="$5"
 
 GOLD_COMMAND="-gold"
 FRONT_COMMAND="-front"
@@ -16,9 +20,5 @@ MACL_COMMAND="-macl"
 EIQ_COMMAND="-eiq"
 USER_COMMAND="-userName"
 
-
-#echo the command
-echo "$LENS $ARGS $GOLD_COMMAND $GOLD $FRONT_COMMAND $FRONT $MACL_COMMAND $MACL $EIQ_COMMAND $EIQ $USER_COMMAND $USER $4" 1>&2
-
 #execute everything
-$LENS $ARGS $GOLD_COMMAND $GOLD $FRONT_COMMAND $FRONT $MACL_COMMAND $MACL $EIQ_COMMAND $EIQ $USER_COMMAND $USER $4 -configuration $5 -data $5
+$LENS $ARGS $GOLD_COMMAND $GOLD $FRONT_COMMAND $FRONT $MACL_COMMAND $MACL $EIQ_COMMAND $EIQ $USER_COMMAND $USER $TYPE -configuration $TEMP -data $TEMP
