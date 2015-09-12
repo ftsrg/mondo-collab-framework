bbab22f671fd2bad87405a366742e663e1a00d10
Csaba Debreceni <debreceni@mit.bme.hu>
2015-09-12 12:01:54 +0200

    Publish lock definition implemented on client side

---
+++ org.mondo.collaboration.security.mpbl.client/src/org/mondo/collaboration/security/mpbl/client/MondoCollaborationClient.java
@@ -0,0 +1,42 @@
+package org.mondo.collaboration.security.mpbl.client;
+
+import org.eclipse.incquery.patternlanguage.emf.eMFPatternLanguage.PatternModel;
+import org.mondo.collaboration.security.mpbl.server.dto.LockDTO;
+import org.mondo.collaboration.security.mpbl.server.dto.ResponseLockDTO;
+import org.mondo.collaboration.security.mpbl.server.impl.PropertyBasedLockingServer;
+
+public class MondoCollaborationClient {
+
+    private static MondoCollaborationClient instance;
+
+    private MondoCollaborationClient() {
+    }
+
+    public static MondoCollaborationClient instance() {
+        if (instance == null) {
+            instance = new MondoCollaborationClient();
+        }
+
+        return instance;
+    }
+
+    public ResponseLockDTO publishLockDefinition(
+            PatternModel model, 
+            String username, 
+            String password,
+            String frontRepository) {
+        
+        return PropertyBasedLockingServer.instance()
+                .publishLockDefinition(new LockDTO(model, username, password, frontRepository));
+    }
+
+    public void lock() {
+    }
+
+    public void unlock() {
+    }
+
+    public void unpublishLockDefinition() {
+    }
+
+}
