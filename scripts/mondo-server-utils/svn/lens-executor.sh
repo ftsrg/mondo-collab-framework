a1fdfcec391ff2f15475869a4a1d8112341f5d68
Csaba Debreceni <debreceni@mit.bme.hu>
2015-11-10 16:38:19 +0100

    fixing scripts for obfuscation

--- scripts/mondo-server-utils/svn/lens-executor.sh
+++ scripts/mondo-server-utils/svn/lens-executor.sh
@@ -15,10 +15,10 @@ log() {
 
 # Config variables
 # Set them carefully
-LENS="/home/mondo/MONDO-Demo/lens/eclipse/MondoOfflineCollaborationLens"
+LENS="$DIR/eclipse/MondoOfflineCollaborationLens"
 ARGS="-nosplash -consoleLog --launcher.suppressErrors -application org.mondo.collaboration.security.lens.offline.cli"
-MACL="/home/mondo/MONDO-Demo/admin/workspace/test.project.rules/src/es/ikerlan/wt/emf/security/example/rules.macl"
-EIQ="/home/mondo/MONDO-Demo/admin/workspace/test.project.rules/src/es/ikerlan/wt/emf/security/example/queries.eiq"
+MACL="$DIR/rules.macl"
+EIQ="$DIR/queries.eiq"
 
 # Input parameters
 USER="$1"
@@ -43,17 +43,19 @@ OBF_PREFIX_COMMAND="-obfuscatorPrefix"
 #execute everything
 log "$LENS $ARGS $GOLD_COMMAND $GOLD $FRONT_COMMAND $FRONT $MACL_COMMAND $MACL $EIQ_COMMAND $EIQ $USER_COMMAND $USER $TYPE -configuration $TEMP -data $TEMP $OBF_SALT_COMMAND $OBF_SALT $OBF_SEED_COMMAND $OBF_SEED $OBF_PREFIX_COMMAND $OBF_PREFIX"
 
-set +e
-$LENS $ARGS $GOLD_COMMAND $GOLD $FRONT_COMMAND $FRONT $MACL_COMMAND $MACL $EIQ_COMMAND $EIQ $USER_COMMAND $USER $TYPE -configuration $TEMP -data $TEMP $OBF_SALT_COMMAND $OBF_SALT $OBF_SEED_COMMAND $OBF_SEED $OBF_PREFIX_COMMAND $OBF_PREFIX > /dev/null 2>&1
-ret=$?
-log "$ret"
 set -e
-if [ $ret -eq 255 ]
-then
-   log "Mondo Exception: Security access violated"
-   echo "Mondo Exception: Security access violated" 1>&2
-   exit 1
-fi
-log "\n\n\n"
 
-set -e
+#set +e
+$LENS $ARGS $GOLD_COMMAND $GOLD $FRONT_COMMAND $FRONT $MACL_COMMAND $MACL $EIQ_COMMAND $EIQ $USER_COMMAND $USER $TYPE -configuration $TEMP -data $TEMP $OBF_SALT_COMMAND $OBF_SALT $OBF_SEED_COMMAND $OBF_SEED $OBF_PREFIX_COMMAND $OBF_PREFIX
+#ret=$?
+#log "$ret"
+#set -e
+#if [ $ret -eq 255 ]
+#then
+#   log "Mondo Exception: Security access violated"
+#   echo "Mondo Exception: Security access violated" 1>&2
+#   exit 1
+#fi
+#log "\n\n\n"
+
+#set -e
