4154e7441081686de4c6f21eb77bc141410f5274
Debreceni Csaba <debreceni@mit.bme.hu>
2016-01-25 18:22:00 +0100

    Lens Activator fixed to read macl and eiq files

--- plugins/org.mondo.collaboration.online.core/src/org/mondo/collaboration/online/core/LensActivator.java
+++ plugins/org.mondo.collaboration.online.core/src/org/mondo/collaboration/online/core/LensActivator.java
@@ -6,16 +6,20 @@ import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Plugin;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.incquery.patternlanguage.emf.EMFPatternLanguageStandaloneSetup;
 import org.eclipse.incquery.runtime.exception.IncQueryException;
 import org.mondo.collaboration.security.lens.bx.online.OnlineCollaborationSession;
 import org.mondo.collaboration.security.lens.bx.online.OnlineCollaborationSession.Leg;
 import org.mondo.collaboration.security.lens.correspondence.EObjectCorrespondence;
+import org.mondo.collaboration.security.macl.xtext.AccessControlLanguageStandaloneSetup;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
@@ -48,6 +52,9 @@ public class LensActivator extends Plugin implements BundleActivator {
 		URL confURL = getBundle().getEntry("log4j.properties");
 		PropertyConfigurator.configure(FileLocator.toFileURL(confURL).getFile());
 
+		AccessControlLanguageStandaloneSetup.doSetup();
+		EMFPatternLanguageStandaloneSetup.doSetup();
+		
 		logger.info(PLUGIN_ID + " is activated");
 	}
 
@@ -71,23 +78,38 @@ public class LensActivator extends Plugin implements BundleActivator {
 		return modelSessions;
 	}
 
-	public static void initializeSession(URI goldURI, ResourceSet resourceSet, StorageAccess sa) {
+	/**
+	 * Initializes an {@link OnlineCollaborationSession} and stores in {@link LensActivator#getModelSessions()} map
+	 * @param goldURI that points to the gold model
+	 * @param sa that provides policy model configuration
+	 */
+	public static void initializeSession(URI goldURI, StorageAccess sa) {
 
 		try {
-			OnlineCollaborationSession onlineCollaborationSession = new OnlineCollaborationSession(goldURI, resourceSet,
-					EObjectCorrespondence.getRegisteredIDProviderFactory(), sa.loadPolicyModel());
+			Resource policyModel = sa.loadPolicyModel();
+			OnlineCollaborationSession onlineCollaborationSession = new OnlineCollaborationSession(goldURI, new ResourceSetImpl(),
+					EObjectCorrespondence.getRegisteredIDProviderFactory(), policyModel);
 			modelSessions.put(goldURI, onlineCollaborationSession);
-//		} catch (IncQueryException | CoreException e) {
-		} catch (Exception e) {
+		} catch (IncQueryException | CoreException e) {
 			logger.error("Error during online session initialization", e);
 		}
 
 	}
 
-	public static Resource getOrCreateResource(URI goldURI, StorageAccess sa) {
+	/**
+	 * Returns user specific front resource in the given resource set.
+	 * @param goldURI
+	 * @param resourceSet will be the container of front resource
+	 * @param sa that provides user specific information
+	 * @return user specific front resource in the given resource set
+	 */
+	public static Resource getOrCreateResource(URI goldURI, ResourceSet resourceSet, StorageAccess sa) {
 		try {
-			Leg leg = getModelSessions().get(goldURI).new Leg(sa.getUsername(), sa.getObfuscator());
-			return leg.getFrontResourceSet().getResources().get(0);
+			Leg leg = getModelSessions().get(goldURI).new Leg(sa.getUsername(), sa.getObfuscator(), 
+					true, resourceSet,OnlineCollaborationSession.FAKE_MAIN_RESOURCE_URI);
+			Resource frontResource = leg.getFrontResourceSet().getResources().get(0);
+			Assert.isNotNull(frontResource);
+			return frontResource;
 		} catch (InvocationTargetException e) {
 			logger.error("Error during lens execution", e);
 		}
