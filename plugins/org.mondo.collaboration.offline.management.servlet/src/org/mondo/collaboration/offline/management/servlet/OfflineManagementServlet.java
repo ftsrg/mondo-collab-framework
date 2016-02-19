2ad71b9aee6df1dbdf0ac7b340b599acbc7aceda
Gabor Bergmann <bergmann@mit.bme.hu>
2016-02-19 04:56:32 -0800

    Added authentication scheme based on Hawk solution

--- plugins/org.mondo.collaboration.offline.management.servlet/src/org/mondo/collaboration/offline/management/servlet/OfflineManagementServlet.java
+++ plugins/org.mondo.collaboration.offline.management.servlet/src/org/mondo/collaboration/offline/management/servlet/OfflineManagementServlet.java
@@ -1,14 +1,20 @@
 package org.mondo.collaboration.offline.management.servlet;
 
-import javax.servlet.Servlet;
+import org.mondo.collaboration.offline.management.servlet.util.RequestAwareThriftServlet;
 
-import org.apache.thrift.protocol.TJSONProtocol;
-import org.apache.thrift.server.TServlet;
+import uk.ac.york.mondo.integration.api.utils.APIUtils.ThriftProtocol;
 
-import uk.ac.york.mondo.integration.api.OfflineCollaboration;
-import uk.ac.york.mondo.integration.api.Users;
-
-public class OfflineManagementServlet extends TServlet implements Servlet {
+/*******************************************************************************
+ * Copyright (c) 2016 Budapest University of Technology and Economics.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Gabor Bergmann - initial API and implementation
+ *******************************************************************************/
+public class OfflineManagementServlet extends RequestAwareThriftServlet {
 
 	/**
 	 * 
@@ -16,9 +22,15 @@ public class OfflineManagementServlet extends TServlet implements Servlet {
 	private static final long serialVersionUID = 6509336680278155129L;
 
 	
-	public OfflineManagementServlet() throws Exception {
-		super(new OfflineCollaboration.Processor<OfflineCollaboration.Iface>(new OfflineCollaborationHandler()), new TJSONProtocol.Factory());
+	public OfflineManagementServlet(OfflineManagementThriftProcessorFactory factory) {
+		super(factory, factory.getProtocol().getProtocolFactory());
 	}
 	
-	// TODO authenticate
+	public OfflineManagementServlet() {
+		this(new OfflineManagementThriftProcessorFactory(ThriftProtocol.JSON));
+	}
+	
+//	public OfflineManagementServlet() throws Exception {
+//		super(new OfflineCollaboration.Processor<OfflineCollaboration.Iface>(new OfflineCollaborationHandler()), new TJSONProtocol.Factory());
+//	}
 }
