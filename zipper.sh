7dad86664dacc9bc85ce4ddd357c463088ee659f
Csaba Debreceni <debreceni@mit.bme.hu>
2016-03-03 11:35:56 +0100

    Zipper script added

---
+++ zipper.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -e
+cp plugins/org.mondo.collaboration.security.lens.offline.daemon.api/target/org.mondo.collaboration.security.lens.offline.daemon.api-0.0.1-SNAPSHOT.jar scripts/mondo/invoker/invoker.jar
+if [ -f scripts/mondo/config/user_front.properties ]; then
+  rm scripts/mondo/config/user_front.properties
+fi
+zip -r offline-scripts.zip scripts/mondo -x *.git*
