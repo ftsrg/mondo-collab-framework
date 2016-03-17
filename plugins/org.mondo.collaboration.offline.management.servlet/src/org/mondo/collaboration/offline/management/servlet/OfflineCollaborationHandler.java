e4f1964a1915a2ce73f9e681cf83d6ab2d6c48e8
Marton Bur <marton.bur@gmail.com>
2016-03-17 10:43:36 +0100

    Server calls repository management scripts #16

--- plugins/org.mondo.collaboration.offline.management.servlet/src/org/mondo/collaboration/offline/management/servlet/OfflineCollaborationHandler.java
+++ plugins/org.mondo.collaboration.offline.management.servlet/src/org/mondo/collaboration/offline/management/servlet/OfflineCollaborationHandler.java
@@ -1,14 +1,18 @@
 package org.mondo.collaboration.offline.management.servlet;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.thrift.TException;
 
 import uk.ac.york.mondo.integration.api.GoldRepoNotFound;
+import uk.ac.york.mondo.integration.api.OfflineCollaboration.Iface;
 import uk.ac.york.mondo.integration.api.OfflineCollaborationInternalError;
 import uk.ac.york.mondo.integration.api.UnauthorizedRepositoryOperation;
 import uk.ac.york.mondo.integration.api.utils.APIUtils.ThriftProtocol;
-import uk.ac.york.mondo.integration.api.OfflineCollaboration.Iface;
 
 /*******************************************************************************
  * Copyright (c) 2016 Budapest University of Technology and Economics.
@@ -37,18 +41,42 @@ public class OfflineCollaborationHandler implements Iface {
 		this(null, null);
 	}
 
+	public static String getScriptsFolder() {
+		String retBundle = Activator.getContext().getBundle().getBundleContext().getProperty("mondo.scripts.folder");
+		String retSystem = System.getProperty("mondo.workspace.folder");
+		return retBundle == null ? retSystem : retBundle;
+	}
+	
 
 	@Override
 	public void regenerateFrontRepositories(String goldRepoURL) throws GoldRepoNotFound,
 			UnauthorizedRepositoryOperation, OfflineCollaborationInternalError, TException {
 		System.out.println("regenerateFrontRepositories call");
 		System.out.println("\tfor repo " + goldRepoURL);
-		if (httpRequest == null)
+		if (httpRequest == null){
 			System.out.println("\trequest is NULL!!!!! ");
-		else if (httpRequest.getUserPrincipal() == null)
+		}
+		else if (httpRequest.getUserPrincipal() == null) {
 			System.out.println("\tuser principal is NULL!!!!! ");
-		else
+		}
+		else {
 			System.out.println("\tby user " + httpRequest.getUserPrincipal().getName());
+		}
+		
+		try {
+			
+			String scriptsFolder = getScriptsFolder();
+			
+			ProcessBuilder builder1 = new ProcessBuilder(scriptsFolder + "/wipe-repositories.sh", "--force");
+			/* Process process1 = */ builder1.start();
+			
+			ProcessBuilder builder2 = new ProcessBuilder(scriptsFolder + "/init-repositories.sh", "--apache2");
+			/* Process process2 = */ builder2.start();
+			
+		} catch (IOException e) {
+			throw new OfflineCollaborationInternalError(e.getMessage());
+		}
+		
 	}
 
 	@Override
@@ -56,13 +84,31 @@ public class OfflineCollaborationHandler implements Iface {
 			UnauthorizedRepositoryOperation, OfflineCollaborationInternalError, TException {
 		System.out.println("getMyFrontRepositoryURL call");
 		System.out.println("\tfor repo " + goldRepoURL);
-		if (httpRequest == null)
+		if (httpRequest == null){
 			System.out.println("\trequest is NULL!!!!! ");
-		else if (httpRequest.getUserPrincipal() == null)
+		}
+		else if (httpRequest.getUserPrincipal() == null){
 			System.out.println("\tuser principal is NULL!!!!! ");
-		else
+		}
+		else {
 			System.out.println("\tby user " + httpRequest.getUserPrincipal().getName());
-		return "fake://url.org";
+		}
+		
+		String frontRepoURL = null;
+		try {
+			String userName = httpRequest.getUserPrincipal().getName();
+			String scriptsFolder = getScriptsFolder();
+			ProcessBuilder processBuilder = new ProcessBuilder(scriptsFolder + "/get-front-repository.sh", userName);
+			Process p = processBuilder.start();
+			BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			frontRepoURL = br.readLine();
+			System.out.println(frontRepoURL);
+		
+		} catch (IOException e) {
+			throw new OfflineCollaborationInternalError(e.getMessage());
+		}
+		
+		return frontRepoURL;
 	}
 
 }
