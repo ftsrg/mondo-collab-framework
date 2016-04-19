914ddf7937e989499eeb875ab4d0fc6e1b8c5d94
Csaba Debreceni <debreceni@mit.bme.hu>
2016-04-19 17:00:00 +0200

    Updated zipper

--- zipper.sh
+++ zipper.sh
@@ -2,7 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-set -e
 cp plugins/org.mondo.collaboration.security.lens.offline.daemon.api/target/org.mondo.collaboration.security.lens.offline.daemon.api-0.0.1-SNAPSHOT-jar-with-dependencies.jar scripts/mondo/invoker/invoker.jar
 if [ -f scripts/mondo/config/user_front.properties ]; then
   rm scripts/mondo/config/user_front.properties
