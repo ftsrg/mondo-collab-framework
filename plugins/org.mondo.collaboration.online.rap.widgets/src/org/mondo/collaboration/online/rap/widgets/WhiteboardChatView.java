3350f4d71e53b230cf48c439adc996b1cda340a9
Marton Bur <marton.bur@gmail.com>
2016-04-26 09:49:07 +0200

    Used different layout classes in the UI to fix #46. Closing.
    Also removed previous jar and sources.

--- plugins/org.mondo.collaboration.online.rap.widgets/src/org/mondo/collaboration/online/rap/widgets/WhiteboardChatView.java
+++ plugins/org.mondo.collaboration.online.rap.widgets/src/org/mondo/collaboration/online/rap/widgets/WhiteboardChatView.java
@@ -26,11 +26,18 @@ import org.mondo.collaboration.online.rap.UISessionManager;
 
 import com.google.common.collect.Maps;
 import com.google.common.util.concurrent.FutureCallback;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Label;
 
 @SuppressWarnings("serial")
 public class WhiteboardChatView extends WhiteboardGenericView {
+	public WhiteboardChatView() {
+	}
 
-	private static final String PLACEHOLDER = "                     ";
+	private static final String PLACEHOLDER = "User currently not in a whiteboard";
 	
 	private static AddChatToWhiteboard addCallback;
 	private static RemoveFromWhiteboard removeCallback;
@@ -48,6 +55,7 @@ public class WhiteboardChatView extends WhiteboardGenericView {
 
 	private volatile static Map<URI, String> messages = Maps.newHashMap();
 	private TableViewer userList;
+	private Text txtMessage;
 
 	private void sendMessage() {
 		String message = txtMessage.getText();
@@ -80,69 +88,64 @@ public class WhiteboardChatView extends WhiteboardGenericView {
 	 */
 	@Override
 	public void createPartControl(Composite parent) {
+
+		super.createPartControl(parent);
+		
 		addCallback.setMessageSource(this);
 		removeCallback.setMessageSource(this);
-		
+				
 		Composite container = new Composite(parent, SWT.NONE);
-		container.setLayout(new BorderLayout());
+		container.setLayout(new GridLayout(2, false));
 		{
-			Composite composite = new Composite(container, SWT.NONE);
-			composite.setLayoutData(BorderLayout.SOUTH);
-			composite.setLayout(new BorderLayout());
+//			Composite composite = new Composite(container, SWT.NONE);
+//			composite.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, true, 1, 1));
+//			composite.setLayout(new GridLayout(1, false));
 
-			txtMessage = new Text(composite, SWT.BORDER);
-			txtMessage.setLayoutData(BorderLayout.CENTER);
-			
-
-			txtMessage.addKeyListener(new KeyAdapter() {
-
-				@Override
-				public void keyPressed(KeyEvent e) {
-					super.keyPressed(e);
-					if (e.keyCode == SWT.CR || e.keyCode == SWT.KEYPAD_CR) { 
-						sendMessage();
-					}
-				}
-
-			});
-
-			Button btnSend = new Button(composite, SWT.None);
-			btnSend.setLayoutData(BorderLayout.EAST);
-			btnSend.setText("Send");
-			btnSend.addMouseListener(new MouseAdapter() {
-				@Override
-				public void mouseUp(MouseEvent e) {
-					super.mouseUp(e);
-					sendMessage();
-				}
-
-			});
-		}
-		{
-			Composite composite = new Composite(container, SWT.NONE);
-			composite.setLayoutData(BorderLayout.CENTER);
-			composite.setLayout(new FillLayout());
-
-			txtMessagePool = new Text(composite, SWT.READ_ONLY | SWT.V_SCROLL | SWT.H_SCROLL);
-			txtMessagePool.setText("");
-		}
-		{
-			Composite composite = new Composite(container, SWT.NONE);
-			composite.setLayoutData(BorderLayout.WEST);
-			composite.setLayout(new FillLayout());
-
-			userList = new TableViewer(composite);
+			userList = new TableViewer(container);
+			Table table = userList.getTable();
+			table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, true, 1, 1));
 			userList.setContentProvider(new UserContentProvider());
 			userList.setLabelProvider(new DecoratingLabelProvider(new UserStatusLabelProvider(), null));
 			userList.setInput(PLACEHOLDER);
+		}
+		txtMessagePool = new Text(container, SWT.READ_ONLY | SWT.V_SCROLL | SWT.H_SCROLL | SWT.BORDER);
+		txtMessagePool.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		{
+			txtMessagePool.setText("");
 		}
 		
 		UINotifierManager.register(UISessionManager.EVENT_USER_ONLINE, RWT.getUISession(), new UpdateCallback());
 		UINotifierManager.register(UISessionManager.EVENT_USER_INACTIVE, RWT.getUISession(), new UpdateCallback());
 		UINotifierManager.register(UISessionManager.EVENT_USER_OFFLINE, RWT.getUISession(), new UpdateCallback());
 		UINotifierManager.register(UISessionManager.EVENT_USER_REMOVED, RWT.getUISession(), new UpdateCallback());
+
 		
-		super.createPartControl(parent);
+		Button btnSend = new Button(container, SWT.None);
+		btnSend.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+		// btnSend.setLayoutData(BorderLayout.EAST);
+		btnSend.setText("Send");
+		btnSend.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseUp(MouseEvent e) {
+				super.mouseUp(e);
+				sendMessage();
+			}
+			
+		});
+		txtMessage = new Text(container, SWT.BORDER);
+		txtMessage.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		txtMessage.addKeyListener(new KeyAdapter() {
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				super.keyPressed(e);
+				if (e.keyCode == SWT.CR || e.keyCode == SWT.KEYPAD_CR) {
+					sendMessage();
+				}
+			}
+
+		});
+
 	}
 
 	@Override
